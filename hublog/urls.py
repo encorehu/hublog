e3029435466d6157a86498460dcf1baf410db0cc
encorehu <huyoo353@126.com>
2014-03-08 16:26:06 +0800

    init hublog

---
+++ hublog/urls.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# python.
+#import datetime
+# ------------------------------------------------------------
+
+# django.
+from django.conf.urls import *
+from django.core.paginator import Paginator, InvalidPage
+from django.views.generic import DetailView, ListView
+
+# ------------------------------------------------------------
+
+#from forms  import AForm,BForm
+# ------------------------------------------------------------
+
+urlpatterns = patterns('',
+
+    #url(r'^$',     'hublog.views.index',name='hublog_index'),
+
+)
